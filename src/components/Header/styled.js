8daa43c7042d0322f8bba0fdbbd2810291948af2
Crystopher Marino <chrismarino23@gmail.com>
2024-11-25 23:26:25 -0300

    entrega 25/11

---
+++ src/components/Header/styled.js
@@ -0,0 +1,9 @@
+// import { styled } from "styled-components";
+
+// export const Header = styled.nav `
+//     width: 100%;
+// 	height: 100vh;
+//     background-color: red;
+//     font-size: 18px;
+//     color: black;
+// `
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { App } from './App.jsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme/default.js'
+import { GlobalStyle } from './styles/global.js'
+import { Theme } from '@radix-ui/themes'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<Theme>
+				<App />
+			</Theme>
+		</ThemeProvider>
+	</StrictMode>
 )
